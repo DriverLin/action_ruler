8bfdc7e6f7504b4ecb2056122b828f3b68c43127
DriverLin <40745915+DriverLin@users.noreply.github.com>
2022-01-10 23:47:25 +0800

    init

---
+++ aliyunpan/auto_refresh/main.py
@@ -0,0 +1,41 @@
+
+import requests
+import json
+from cryptography.fernet import Fernet 
+import base64
+import os
+import hashlib
+
+
+
+def enc(key, data):
+    md5hash = hashlib.md5(key.encode()).hexdigest().encode('utf-8')
+    f = Fernet(base64.urlsafe_b64encode(md5hash))
+    return str(f.encrypt(data.encode('utf-8')), 'utf-8')
+
+
+def dec(key, data):
+    md5hash = hashlib.md5(key.encode()).hexdigest().encode('utf-8')
+    f = Fernet(base64.urlsafe_b64encode(md5hash))
+    return f.decrypt(data.encode('utf-8')).decode('utf-8')
+
+
+
+if __name__ == '__main__':
+    try:
+        old_refresh_token = dec(os.environ.get("KEY"),open("./refresh_token.bin").read())
+        # print("old_refresh_token: " , old_refresh_token)
+        adata = {
+            "refresh_token":old_refresh_token
+        }
+        aheaders = {'Content-Type': 'application/json'}
+        url = "https://api.aliyundrive.com/token/refresh"
+        response = requests.post(url, headers=aheaders, data = json.dumps(adata))
+        new_refresh_token = response.json()["refresh_token"]
+        # print("new_refresh_token: " , new_refresh_token)
+        with open("./refresh_token.bin", "w") as f:
+            f.write( enc(os.environ.get("KEY"),new_refresh_token))
+        print("refresh token refreshed successfully")
+    except Exception as e:
+        print("Error: ", e)
+    
---
+++ aliyunpan/backup_onedrive/main.py
@@ -0,0 +1,51 @@
+import requests
+import json
+from cryptography.fernet import Fernet 
+import base64
+import os
+import hashlib
+import threading
+
+
+def dec(key, data):
+    md5hash = hashlib.md5(key.encode()).hexdigest().encode('utf-8')
+    f = Fernet(base64.urlsafe_b64encode(md5hash))
+    return f.decrypt(data.encode('utf-8')).decode('utf-8')
+
+def download_newleast(releaseUrl):
+    try:
+        print("fetching release from ", releaseUrl)
+        result = requests.get(releaseUrl).json()
+        print("fetching release done")
+       
+        for asset in result["assets"]:
+            name = asset["name"]
+            if name.endswith(".jar"):
+                print("Downloading " + name + " from " + asset["browser_download_url"])
+                file = requests.get(asset["browser_download_url"])
+                print("Downloaded " + name + " successfully")
+                return file.content
+        return None
+    except Exception as e:
+        print(e)
+        return None
+try:
+    tokenbin_url = "https://raw.githubusercontent.com/DriverLin/Refresh_token_autorefresh/main/refresh_token.bin"
+    response = requests.get(tokenbin_url)
+    refresh_token = dec(os.environ.get("KEY") ,response.text)
+    jar_path = os.path.join(os.getcwd(), "webdav-aliyundriver.jar")
+    bytes = download_newleast("https://api.github.com/repos/zxbu/webdav-aliyundriver/releases/latest")
+    with open(jar_path, "wb") as f:
+        f.write(bytes)
+    cmd = "java -jar {} --aliyundrive.refresh-token={} --server.port=8900 --aliyundrive.auth.enable=false".format(jar_path,refresh_token)
+
+    threading.Thread(target=os.system, args=(cmd,)).start()
+
+    os.system("chmod 777 ./rclone")
+    os.system("./rclone --config ./rclone.conf copy onedrive2: aliyunenc: -P --stats=3s --stats-one-line")
+except Exception as e:
+    print(e)
+exit()
+
+
+
