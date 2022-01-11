beea33ebff57ffdf56d1f0512f4aab6f8334fa90
DriverLin <40745915+DriverLin@users.noreply.github.com>
2022-01-11 10:05:36 +0800

    0

--- report_macrodroid.py
+++ report_macrodroid.py
@@ -6,18 +6,18 @@ import os
 
 if __name__ == '__main__':
     try:
-        apiurl = os.environ.get("URL")
+        token = os.environ.get("TOKEN")
         tag=os.environ.get("TAG")
         title=os.environ.get("TITLE")
         text = os.environ.get("TEXT")
         print("tag: " + tag)
         print("title: " + title)
         print("text: " + text)
-        url = urllib.parse.quote("{}?tag={}&title={}&text={}".format(apiurl, tag, title, text), safe=string.printable)
+        url = urllib.parse.quote("https://trigger.macrodroid.com/{}?tag={}&title={}&text={}".format(token, tag, title, text), safe=string.printable)
         req = urllib.request.Request(url)
         url_open = urllib.request.urlopen(req)
         data = url_open.read()
         print(data)
     except Exception as e:
         print("Error: ", e)
-        
+
