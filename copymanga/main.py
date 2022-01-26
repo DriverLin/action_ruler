cb5d3475bea60af1d81bc837a2a9ed94bb7d1dfd
DriverLin <40745915+DriverLin@users.noreply.github.com>
2022-01-26 21:17:51 +0800

    Update main.py

--- copymanga/main.py
+++ copymanga/main.py
@@ -177,9 +177,9 @@ def copymanga_download(manga_id, save_name=None, save_path=r"./"):
 #=======================================================================================================================
 cache = json.load(open(r"cache.json", "r", encoding="utf-8"))
 watchList = json.load(open(r"watching.json", "r", encoding="utf-8"))
-os.system("mkdir /mnt/managaMountPoint")
-os.system("nohup rclone --config ./rclone.conf mount onedrive:Manga  /mnt/managaMountPoint --vfs-cache-mode full &")
+os.system("mkdir /tmp/manga")
+os.system("nohup rclone --config ./rclone.conf mount onedrive:Manga  /tmp/manga --vfs-cache-mode full &")
 sleep(5)
 for (mid,mname) in watchList:
-    copymanga_download(mid, mname, "/mnt/managaMountPoint")
+    copymanga_download(mid, mname, "/tmp/manga")
 json.dump(cache, open(r"cache.json", "w", encoding="utf-8"))
